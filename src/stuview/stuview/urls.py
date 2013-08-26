177f57ff1ac7f8e09778389e9c058e6270a066a3
Steve Komarov <skomarov@edx.org>
2013-08-26 13:19:50 -0400

    adding webrtc4all

--- src/stuview/stuview/urls.py
+++ src/stuview/stuview/urls.py
@@ -20,6 +20,7 @@ urlpatterns = patterns(
     url(r'^bench', 'bench', name='bench'),
     url(r'^queue', 'queue', name='queue'),
     url(r'^canvas', 'canvas', name='canvas'),
+    url(r'^audio', 'audio', name='audio'),
     url(r'^populate', 'populate', name='populate'),
     url(r'^cleardb', 'cleardb', name='cleardb'),
     url(r'^allqinfo', 'allqinfo', name='allqinfo'),
