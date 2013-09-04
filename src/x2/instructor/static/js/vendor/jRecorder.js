95c50016a351b5dd008c21b910ed614da5731f02
Steve Komarov <skomarov@edx.org>
2013-09-04 18:00:21 -0400

    neiz jrec

--- src/x2/instructor/static/js/vendor/jRecorder.js
+++ src/x2/instructor/static/js/vendor/jRecorder.js
@@ -10,13 +10,6 @@
  * Author: Sajith Amma
  * Version: 1.1
  * Date: 14 December 2011
-
-Version changes
-
-Added preview option right after recording.
-Added seperate function sendData to trigger to send data to server (it won't send automcatically to server)
-
-
  */
 
 /* Code is not verified using http://www.jshint.com/ */
@@ -72,6 +65,7 @@ Added seperate function sendData to trigger to send data to server (it won't sen
 						'bgcolor': '#ff0000',
 						'swf_path': 'jRecorder.swf',
 						'host': 'acceptfile.php?filename=hello.wav',
+						'token': '',
 						'callback_started_recording' : function(){},
 						'callback_finished_recording' : function(){},
 						'callback_stopped_recording': function(){},
@@ -93,14 +87,13 @@ Added seperate function sendData to trigger to send data to server (it won't sen
 		
 						jRecorderSettings = settings;
 						
-
-        var ie10 = /MSIE (\d+)/.exec(navigator.userAgent)
-
-							if( ie10 != null && parseInt(ie10[1])<=8) {
+						
+						
+							if($.browser.msie && Number($.browser.version) <= 8) {
 							var objStr = '<object  name="'+ settings['recorder_name'] +'" id="' + settings['recorder_id'] + '" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="'+ settings['rec_width'] +'" height="'+ settings['rec_height']+'"></object>';
 
 							var paramStr = [
-								'<param name="movie" value="'+ settings['swf_path'] + '?host=' + settings['host'] +  '" />',
+								'<param name="movie" value="'+ settings['swf_path'] + '?host=' + settings['host'] + settings['token'] + '" />',
 								
 								'<param name="allowScriptAccess" value="always" />',
 								'<param name="bgcolor" value="' + settings['bgcolor'] + '" />',
@@ -128,7 +121,7 @@ Added seperate function sendData to trigger to send data to server (it won't sen
 							htmlObj = document.createElement("object");
 							htmlObj.setAttribute("id", settings['recorder_id'] );
 							htmlObj.setAttribute("name", settings['recorder_name'] );
-							htmlObj.setAttribute("data",  settings['swf_path'] + '?host=' + settings['host'] );
+							htmlObj.setAttribute("data",  settings['swf_path'] + '?host=' + settings['host']+ settings['token'] );
 							htmlObj.setAttribute("type", "application/x-shockwave-flash");
 							htmlObj.setAttribute("width", settings['rec_width']); // Non-zero
 							htmlObj.setAttribute("height", settings['rec_height']); // Non-zero
@@ -146,7 +139,7 @@ Added seperate function sendData to trigger to send data to server (it won't sen
 						var divObj = document.createElement("div");
 						
 						divObj.setAttribute("id", settings['recorderlayout_id']);
-						divObj.setAttribute("style", "position:absolute;top:"+ settings['rec_top'] +";left:"+ settings['rec_left'] +";z-index:-1");
+						divObj.setAttribute("style", "position:fixed;top:"+ settings['rec_top'] +";left:"+ settings['rec_left'] +";visibility:hidden;");
 						
 						divObj.appendChild(htmlObj);
 						
@@ -163,16 +156,40 @@ Added seperate function sendData to trigger to send data to server (it won't sen
 	};
 	
 	//function call to start a recording
-	$.jRecorder.record = function(max_time){
+	$.jRecorder.record = function(max_time,callback){
+
+
+		//change z-index to make it top
+		$(  '#' + jRecorderSettings['recorderlayout_id'] ).css({
+			'z-index': 99999,
+			visibility:'visible'
+		});
+		getFlashMovie(jRecorderSettings['recorder_name']).jStartRecording(max_time);
 		
+	} 
+
+	//function call to stop recording					
+	$.jRecorder.playPreview = function(callback){
 		
-								//change z-index to make it top
-								$(  '#' + jRecorderSettings['recorderlayout_id'] ).css('z-index', 1000);
-								getFlashMovie(jRecorderSettings['recorder_name']).jStartRecording(max_time);
+		if (typeof callback == 'function') 
+		{
+			$.jRecorder.callback_started_preview = callback;
+		};
+		getFlashMovie(jRecorderSettings['recorder_name']).jPreview();
 							
+	} 
+	$.jRecorder.getUploadToken = function(){
+		return jRecorderSettings['token'];
+	} 
+	$.jRecorder.getId = function(){
+		return jRecorderSettings['recorderlayout_id'];
+	} 
+
+	$.jRecorder.stopPreview = function(){
+					
+		getFlashMovie(jRecorderSettings['recorder_name']).jStopPreview();
 							
-		
-						} 
+	} 
 
 	//function call to stop recording					
 	$.jRecorder.stop = function(){
@@ -182,10 +199,14 @@ Added seperate function sendData to trigger to send data to server (it won't sen
 	} 
 		
 	//function call to send wav data to server url from the init configuration					
-	$.jRecorder.sendData = function(){
-					
+	$.jRecorder.sendData = function(callback){
+		
+		if (typeof callback == 'function') 
+		{
+			jRecorderSettings['callback_finished_sending'] = callback;
+		};
+
 		getFlashMovie(jRecorderSettings['recorder_name']).jSendFileToServer();
-							
 	} 
 	
 	$.jRecorder.callback_started_recording = function(){
@@ -214,9 +235,9 @@ Added seperate function sendData to trigger to send data to server (it won't sen
 	}
 	
 	
-	$.jRecorder.callback_finished_sending = function(){
+	$.jRecorder.callback_finished_sending = function(msg){
 		
-		jRecorderSettings['callback_finished_sending']();
+		jRecorderSettings['callback_finished_sending'](msg);
 		
 	}
 	
@@ -229,7 +250,10 @@ Added seperate function sendData to trigger to send data to server (it won't sen
 	$.jRecorder.callback_activityTime = function(time){
 		
 		//put back flash while recording
-		$(  '#' + jRecorderSettings['recorderlayout_id'] ).css('z-index', -1);
+		$(  '#' + jRecorderSettings['recorderlayout_id'] ).css({
+			'visibility': 'hidden',
+			'z-index':0
+		});
 		
 		jRecorderSettings['callback_activityTime'](time);
 		
