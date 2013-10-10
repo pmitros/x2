7219ab98b418485df9a6e75cda75b7c5c31fc678
Piotr Mitros <pmitros@mitx.mit.edu>
2013-10-10 14:42:52 -0400

    Adding class list

---
+++ src/x2/marketing/bootstrap.py
@@ -0,0 +1,6 @@
+from models import create_course
+
+create_course(slug = '8.01', 
+              name = 'Physics I: Classical Mechanics')
+create_course(slug = '2.01', 
+              name = 'Structures and Materials')
--- src/x2/marketing/models.py
+++ src/x2/marketing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+def create_course(slug, name):
+    c = Course()
+    c.name = name
+    c.slug = slug
+    c.save()
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=32)
+    
