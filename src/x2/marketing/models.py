b3bef78acb4965aec2937b81245d42101cd0a37f
Piotr Mitros <pmitros@mitx.mit.edu>
2013-10-23 12:08:11 -0400

    Architecutre

--- src/x2/marketing/models.py
+++ src/x2/marketing/models.py
@@ -1,11 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 def create_course(slug, name):
-    c = Course()
+    c = Resource()
     c.name = name
     c.slug = slug
     c.save()
 
-class Course(models.Model):
+class Resource(models.Model):
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length=32, unique = True)
+    parent = models.ForeignKey("Resource", default=None) # e.g. for a section, 
+    
+
+class Enrollment(models.Model):
+    user = models.ForeignKey(User)
+    resource = models.ForeignKey(Resource)
+    MODES = (('A', 'Active'),
+             ('L', 'Lurker'), 
+             ('U', 'Unenrolled'))
+    mode = models.CharField(max_length=100, choices=MODES, default = 'Active')
+
